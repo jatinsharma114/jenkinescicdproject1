37ba66fe6f73b67df42d70b7a2b0cf47346b2f3e
jatin sharma <jatin2010sharma@gmail.com>
2023-09-23 19:48:42 +0530

    first commit

---
+++ src/main/java/com/oscarzoon/projectjenkines/ProjectjenkinesApplication.java
@@ -0,0 +1,14 @@
+package com.oscarzoon.projectjenkines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectjenkinesApplication {
+
+	public static void main(String[] args) {
+		//Adding first for commit
+		SpringApplication.run(ProjectjenkinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oscarzoon/projectjenkines/ProjectjenkinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscarzoon.projectjenkines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectjenkinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
